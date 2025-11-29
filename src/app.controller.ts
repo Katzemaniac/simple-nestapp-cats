5593bff1222857942a292b2d34f9dff53c13e423
RedKatzen <gabrieldeavilaf@gmail.com.br>
2025-11-29 10:08:45 -0300

    more about cats, now with id

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
-// import type { Request } from 'express';
-
-const breeds: Array<string> = ['Siamese', 'Persian', 'Maine Coon', 'Bengal'];
+import { Controller, Get, HttpCode, Param, Post, Req } from '@nestjs/common';
+import type { Request } from 'express';
 
 // decorator defining this class to be a controller, providing necessary metadata
 @Controller('cats') // specifying a path prefix, grouping related routes to manipulate 'cats' entity (optional)
 export class AppController {
   @Get() // endpoint defined by HTTP request method and prefix 'cats'
-  findAll(): string {
+  findAll(@Req() request: Request): string { // access the request's details (you can use @Body to access directly)
     return 'Return all cats';
   }
 
-  @Get('breeds')
-  findAllBreeds(): Array<string> {
-    return breeds;
+  @Post()
+  // default status code is 200, for POST is 201
+  @HttpCode(204) // but you can change manually
+  create(): string {
+    return 'This action add a new cat';
+  }
+
+  @Get(':id') // define a route parameter token in the GET decorator
+  findOne(@Param('id') id: string): string { // @Param() decorator make the route parameters (like id) accessible as properties
+    return `This action return a cat of id ${id}`; // or you can pass a specific parameter token to the decorator
   }
 }
